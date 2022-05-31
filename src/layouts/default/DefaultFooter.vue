82916774b619f1631b70d81f3ae9865c3c321f82
Shadman Ahsan <shadman_ahsan@yahoo.com>
2022-05-31 23:42:34 +0600

    Initial commit

---
+++ src/layouts/default/DefaultFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <span>Developed by</span> <a href="https://github.com/Shadman-2021" target="_blank" rel="noopener">@Shadman-2021</a>
+    </div>
+  </footer>
+</template>
